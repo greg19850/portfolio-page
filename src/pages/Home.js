e9fabe894f343bd142a7e9b1a2fd1284facbb884
Greg <greg19850@gmail.com>
2023-04-16 18:38:16 +0100

    home welcome message styling corrected

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,14 +8,14 @@ function Home() {
   return (
     <div className="home">
       <div className="home-content">
-        <div className="text top">
+        <div className="text">
           <div>
             <h1 className="span">Hello.</h1>
             <p className="span">My name is Greg, I am software developer based in Coventry, UK.</p>
             <p className="span"> My passions are technology, innovation, and self-develpment. I like turning ideas in real life products.</p>
           </div>
         </div>
-        <div className="links bottom">
+        <div className="links">
           <Link to='/projects'>Go to Projects</Link>
         </div>
       </div>
